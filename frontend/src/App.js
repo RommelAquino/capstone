0a3420b8b7e3a60347c9d0bb33624954e69e2c52
RommelAquino <rommel.h.aquino@gmail.com>
2020-08-28 14:03:23 -0700

    render products to the page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,104 +1,61 @@
 import React from 'react';
-import logo from './logo.svg';
+import data from './data';
 import './App.css';
 
 function App() {
-  return (
-    <div class ='grid-container'>
-        <header class="header">
-            <div class="brand">
-                <button onclick="openMenu()">
-                    &#9776;
-                </button>
-                <a href="index.html">Cardboard Collectors</a>
-            </div>
-            <div class="header-links">
-                <a href="cart.html">Cart</a>
-                <a href="signin.html">Sign In</a>
-            </div>
-        </header>
 
-        <aside class="sidebar">
-            <h3>Shopping Categories</h3>
-            <button class="sidebar-close-button" onclick="closeMenu()"> X </button>
-            <ul>
-                <li><a href="index.html">Pants</a></li>
-                <li><a href="index.html">Shirts</a></li>
-            </ul>
-        </aside>
-       
-        <main class="main">
-            <div class="content">
-                <ul class="products">
-                    <li>
-                        <img src="./frontend/images/curry.jpg" alt="Curry">
-                        <div class="product-name">
-                            <a href="">Stephen Curry</a>
-                            </div>
-                        <div class="product-team">Warriors</div>
-                        <div class="product-price">$1000</div>
-                    </li>
-                    <li>
-                        <img src="./frontend/images/curry.jpg" alt="Curry">
-                        <div class="product-name">
-                            <a href="">Stephen Curry</a>
-                            </div>
-                        <div class="product-team">Warriors</div>
-                        <div class="product-price">$1000</div>
-                    </li>
-                    <li>
-                        <img src="./frontend/images/curry.jpg" alt="Curry">
-                        <div class="product-name">
-                            <a href="">Stephen Curry</a>
-                            </div>
-                        <div class="product-team">Warriors</div>
-                        <div class="product-price">$1000</div>
-                    </li>
-                    <li>
-                        <img src="./frontend/images/curry.jpg" alt="Curry">
-                        <div class="product-name">
-                            <a href="">Stephen Curry</a>
-                            </div>
-                        <div class="product-team">Warriors</div>
-                        <div class="product-price">$1000</div>
-                    </li>
-                    <li>
-                        <img src="./frontend/images/curry.jpg" alt="Curry">
-                        <div class="product-name">
-                            <a href="">Stephen Curry</a>
-                            </div>
-                        <div class="product-team">Warriors</div>
-                        <div class="product-price">$1000</div>
-                    </li>
-                    <li>
-                        <img src="./frontend/images/curry.jpg" alt="Curry">
-                        <div class="product-name">
-                            <a href="">Stephen Curry</a>
-                            </div>
-                        <div class="product-team">Warriors</div>
-                        <div class="product-price">$1000</div>
-                    </li>
-                    <li>
-                        <img src="./frontend/images/curry.jpg" alt="Curry">
-                        <div class="product-name">
-                            <a href="">Stephen Curry</a>
-                            </div>
-                        <div class="product-team">Warriors</div>
-                        <div class="product-price">$1000</div>
-                    </li>
-                    <li>
-                        <img src="./frontend/images/curry.jpg" alt="Curry">
-                        <div class="product-name">
-                            <a href="">Stephen Curry</a>
-                            </div>
-                        <div class="product-team">Warriors</div>
-                        <div class="product-price">$1000</div>
-                    </li>
-                </ul>
-            </div>
-        </main>
-        <footer class="footer">
-            All rights reserved.
+  const openMenu = () => {
+    document.querySelector(".sidebar").classList.add("open");
+  }
+
+  const closeMenu = () => {
+    document.querySelector(".sidebar").classList.remove("open")
+  }
+  return (
+    <div classNameName='grid-container'>
+      <header className="header">
+        <div className="brand">
+          <button onClick={openMenu}>
+            &#9776;
+                </button>
+          <a href="index.html">Cardboard Collectors</a>
+        </div>
+        <div className="header-links">
+          <a href="cart.html">Cart</a>
+          <a href="signin.html">Sign In</a>
+        </div>
+      </header>
+
+      <aside className="sidebar">
+        <h3>Shopping Categories</h3>
+        <button className="sidebar-close-button" onClick={closeMenu}> X </button>
+        <ul>
+          <li><a href="index.html">Pants</a></li>
+          <li><a href="index.html">Shirts</a></li>
+        </ul>
+      </aside>
+
+      <main className="main">
+        <div className="content">
+          <ul className="products">
+            {
+              data.products.map(product =>
+                <li>
+                  <img src={product.image} alt="Curry" />
+                  <div className={product.name}>
+                    <a href="product.html">{product.name}</a>
+                  </div>
+                  <div className="product-team">{product.team}</div>
+                  <div className="product-price">${product.price}</div>
+                </li>)
+            }
+
+
+          </ul>
+        </div>
+      </main>
+      <footer className="footer">
+        All rights reserved.
         </footer>
     </div>
   );
---
+++ frontend/src/data.js
@@ -0,0 +1,32 @@
+export default {
+    products: [
+        {
+            name: 'Slim Shirt',
+            category: 'Shirts',
+            image: '/images/currypsa10.jpg',
+            team: 'Golden State Warriors',
+            price: 1000
+        },
+        {
+            name: 'Slim Shirt',
+            category: 'Shirts',
+            image: '/images/currypsa10.jpg',
+            team: 'Golden State Warriors',
+            price: 1000
+        },
+        {
+            name: 'Slim Shirt',
+            category: 'Pants',
+            image: '/images/currypsa10.jpg',
+            team: 'Golden State Warriors',
+            price: 1234
+        },
+        {
+            name: 'Slim Shirt',
+            category: 'Shirts',
+            image: '/images/currypsa10.jpg',
+            team: 'Golden State Warriors',
+            price: 1000
+        }
+    ]
+}
