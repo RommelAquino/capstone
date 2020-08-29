d0a8224e45cd127026f0de288703c3be6a8c2c43
RommelAquino <rommel.h.aquino@gmail.com>
2020-08-28 22:44:39 -0700

    added product routes

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,6 +1,10 @@
 const router = require('express').Router();
 const controller = require('../controllers/productController')
 
+router.get('/product/:id', controller.show)
 router.get('/', controller.index)
+router.post('/', controller.create)
+router.put('/product/:id', controller.update)
+router.delete('/product/:id', controller.destroy)
 
 module.exports = router;
